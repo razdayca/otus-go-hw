d28fa848d70149765b6682f52aa3dc8e9676bf52
Ilya Razdajbeden <ilya.razdaybeden@gomobile.ru>
2023-10-13 21:48:45 +0300

    init: hw

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,36 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"fmt"
+	"strconv"
+	"strings"
+	"unicode"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(unpackStr string) (string, error) {
+	var final strings.Builder
+	final.Grow(32)
+	for i, symbol := range []rune(unpackStr) {
+		if unicode.IsDigit(symbol) && !unicode.IsDigit([]rune(unpackStr)[i+1]) {
+			count, _ := strconv.Atoi(string(symbol))
+			if count > 0 && i > 0 {
+				fmt.Println(count)
+				final.WriteString(strings.Repeat(string([]rune(unpackStr)[i-1]), count-1))
+			} else if i > 0 {
+				_, buf, _ := strings.Cut(final.String(), string([]rune(unpackStr)[i-1]))
+				final.Reset()
+				final.WriteString(buf)
+			} else {
+				return final.String(), nil
+			}
+
+		} else if unicode.IsDigit(symbol) && unicode.IsDigit([]rune(unpackStr)[i+1]) {
+			return final.String(), nil
+		} else {
+			final.WriteString(string(symbol))
+		}
+	}
+	return final.String(), nil
 }
--- hw02_unpack_string/unpack_test.go
+++ hw02_unpack_string/unpack_test.go
@@ -14,7 +14,11 @@ func TestUnpack(t *testing.T) {
 	}{
 		{input: "a4bc2d5e", expected: "aaaabccddddde"},
 		{input: "abccd", expected: "abccd"},
+		{input: "3abc", expected: ""},
+		{input: "45", expected: ""},
+		{input: "aaa10b", expected: ""},
 		{input: "", expected: ""},
+		{input: "d\n5abc", expected: "d\n\n\n\n\nabc"},
 		{input: "aaa0b", expected: "aab"},
 		// uncomment if task with asterisk completed
 		// {input: `qwe\4\5`, expected: `qwe45`},
