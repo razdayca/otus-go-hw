7cccc3d098309031b6701b49434cc3092cd387f7
Ilya Razdajbeden <ilya.razdaybeden@gomobile.ru>
2023-12-10 23:48:25 +0300

    fix: tests, lint

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -4,7 +4,6 @@ import (
 	"errors"
 	"github.com/cheggaaa/pb/v3"
 	"io"
-	"log"
 	"os"
 )
 
@@ -20,7 +19,12 @@ func Copy(fromPath, toPath string, offset, limit int64) error {
 	}
 	fileStat, _ := os.Stat(fromPath)
 	defer fileFromCopy.Close()
-	bar := pb.Full.Start64(fileStat.Size() - limit - offset)
+	var bar *pb.ProgressBar
+	if limit < fileStat.Size() {
+		bar = pb.Full.Start64(limit)
+	} else {
+		bar = pb.Full.Start64(fileStat.Size() - offset)
+	}
 	barReader := bar.NewProxyReader(fileFromCopy)
 	defer bar.Finish()
 	err = os.MkdirAll(toPath, 0755)
@@ -32,25 +36,29 @@ func Copy(fromPath, toPath string, offset, limit int64) error {
 		return errors.New("can not create file")
 	}
 	defer fileToCopy.Close()
-	io.Copy(fileToCopy, barReader)
+
 	if offset > 0 {
 		if offset > fileStat.Size() {
 			return ErrOffsetExceedsFileSize
 		}
 		_, err = fileFromCopy.Seek(offset, io.SeekStart)
 		if err != nil {
-			log.Fatal(err)
+			defer fileToCopy.Close()
 		}
 	}
 
-	if limit > 0 && limit < fileStat.Size() {
-		if _, err := io.CopyN(fileToCopy, fileFromCopy, limit); err != nil {
+	if limit > int64(0) {
+		if _, err := io.CopyN(fileToCopy, barReader, limit); err != nil {
+			if err == io.EOF {
+				fileToCopy.Sync()
+				return nil
+			}
 			return err
 		}
 		fileToCopy.Sync()
 		return nil
 	} else {
-		if _, err := io.Copy(fileToCopy, fileFromCopy); err != nil {
+		if _, err := io.Copy(fileToCopy, barReader); err != nil {
 			return err
 		}
 		fileToCopy.Sync()
