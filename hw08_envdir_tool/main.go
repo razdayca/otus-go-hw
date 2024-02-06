5f55b2154ed3af84f256d0f4f0464f0ed098302c
Ilya Razdajbeden <ilya.razdaybeden@gomobile.ru>
2024-02-06 17:18:09 +0300

    fix: test

--- hw08_envdir_tool/main.go
+++ hw08_envdir_tool/main.go
@@ -6,18 +6,15 @@ import (
 )
 
 func main() {
-	if len(os.Args) == 1 {
-		fmt.Print("directory is not exist")
-		os.Exit(1)
-	}
+	envDir := os.Args[1]
+	cmd := os.Args[2:]
 
-	d := os.Args[1]
-	env, err := ReadDir(d)
+	env, err := ReadDir(envDir)
 	if err != nil {
 		fmt.Println(err)
-		os.Exit(1)
+		return
 	}
 
-	exitCode := RunCmd(os.Args[2:], env)
-	os.Exit(exitCode)
+	cmdExitCode := RunCmd(cmd, env)
+	os.Exit(cmdExitCode)
 }
