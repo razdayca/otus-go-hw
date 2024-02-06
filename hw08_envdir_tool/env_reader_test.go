6559a26235a20c5303c20fe8e811a05b5a0e1113
Ilya Razdajbeden <ilya.razdaybeden@gomobile.ru>
2024-02-06 17:27:32 +0300

    fix: test

--- hw08_envdir_tool/env_reader_test.go
+++ hw08_envdir_tool/env_reader_test.go
@@ -12,8 +12,10 @@ func TestReadDir(t *testing.T) {
 		expectedEnv := Environment{
 			"ADDED": EnvValue{Value: "from original env", NeedRemove: false},
 			"BAR":   EnvValue{Value: "bar", NeedRemove: false},
+			"EMPTY": EnvValue{Value: "", NeedRemove: true},
 			"FOO":   EnvValue{Value: "   foo\nwith new line", NeedRemove: false},
 			"HELLO": EnvValue{Value: "\"hello\"", NeedRemove: false},
+			"UNSET": EnvValue{Value: "", NeedRemove: true},
 		}
 
 		env, err := ReadDir("testdata/env")
